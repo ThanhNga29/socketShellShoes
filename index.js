30ed47427e2982068ed059fdc6d7da25613950b1
ThanhNga <lethithanhnga291202@gmail.com>
2023-12-11 15:44:34 +0700

    1st commit

---
+++ index.js
@@ -0,0 +1,38 @@
+const io = require("socket.io")(8800, {
+  cors: {
+    origin: ["http://localhost:5000", "http://localhost:4000"],
+  },
+});
+let activeUsers = [];
+io.on("connection", (socket) => {
+  //add new user
+  socket.on("new-add-user", (newUserId) => {
+    // if user is not added previously
+    if (!activeUsers.some((user) => user.userId === newUserId)) {
+      activeUsers.push({
+        userId: newUserId,
+        socketId: socket.id,
+      });
+    }
+    console.log("Connected user", activeUsers);
+
+    io.emit("get-users", activeUsers);
+  });
+  // send message to a specific user
+  socket.on("send-message", (data) => {
+    const { receiverId } = data;
+    console.log("activeUser", activeUsers);
+    const user = activeUsers?.find((user) => user.userId === receiverId);
+    console.log("Sending from socket to :", receiverId);
+    console.log("Data: ", data);
+    console.log("user:", user);
+    if (user) {
+      io.to(user.socketId).emit("recieve-message", data);
+    }
+  });
+  socket.on("disconnect", () => {
+    activeUsers = activeUsers.filter((user) => user.socketId !== socket.id);
+    console.log("User disconnected", activeUsers);
+    io.emit("get-users", activeUsers);
+  });
+});
